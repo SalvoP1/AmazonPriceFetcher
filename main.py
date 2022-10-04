7f5c44789d12245fe35dade8f250b0dadfc3651d
SalvoP1 <SalvatoreP1@web.de>
2022-10-04 09:25:23 +0200

    Code Cleanup with Daniel

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
 
-URL_List = [
+urlList = [
     'https://www.amazon.de/Python-Programmieren-f%C3%BCr-Anf%C3%A4nger-Schritt-f%C3%BCr-Schritt-Anleitungen/dp''/B0B7CR66J2/ref=sr_1_1?keywords=python+programmieren+f%C3%BCr+anf%C3%A4nger&qid=1664736500&qu''=eyJxc2MiOiIyLjg0IiwicXNhIjoiMi41NSIsInFzcCI6IjIuMjYifQ%3D%3D&sprefix=python+programmier%2Caps%2C91&sr=8-1 ',
     'https://www.amazon.de/Automate-Boring-Stuff-Python-2nd/dp/1593279922/ref=''sr_1_1?keywords=automate+the+boring+stuff+with+python&qid=1664744667&qu=''eyJxc2MiOiIxLjg1IiwicXNhIjoiMS45NyIsInFzcCI6IjEuNjQifQ%3D%3D&sprefix=automate+the+%2Caps%2C114&sr=8-1',
     'https://www.amazon.de/Pragmatic-Programmer-journey-mastery-Anniversary/dp/0135957052/ref=sr_1_1?''keywords=the+pragmatic+programmer&qid=1664744779&qu=eyJxc2MiOiIxLjkyIiwicXNhIjoiMS4xNCIsInFzcCI6IjEuMDAifQ%3D%3D&sprefix=the+pragmatic%2Caps%2C90&sr=8-1'
@@ -23,37 +23,31 @@ myHeaders = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:105.0) 
 
 
 def requestWebsite(URL):
-    websiteOfProduct = requests.get(URL, headers=myHeaders)
-    return websiteOfProduct
+    return requests.get(URL, headers=myHeaders)
 
 
 def parseWebsite(websiteOfProduct):
-    contentOfWebsite = BeautifulSoup(websiteOfProduct.content, "html.parser")
-    return contentOfWebsite
+    return BeautifulSoup(websiteOfProduct.content, "html.parser")
 
 
 def getTitleFormProduct(contentOfWebsite):
-    title = contentOfWebsite.find(id="productTitle").get_text()
-    return title
+    return contentOfWebsite.find(id="productTitle").get_text()
 
 
-def getPriceFromProduct(contentOfWebsite):
-    price = contentOfWebsite.find(id="price").get_text()
-    return price
+def fetchPriceFromProduct(parsedWebsite):
+    return parsedWebsite.find(id="price").get_text()
 
 
-def fetchPriceFromProduct(URL):
-    price = getPriceFromProduct(parseWebsite(requestWebsite(URL)))
-    return price
-
-
-def fetchTitleFromProduct(URL):
-    title = getTitleFormProduct(parseWebsite(requestWebsite(URL)))
+def fetchTitleFromProduct(parsedWebsite):
+    title = parsedWebsite.find(id="productTitle").get_text()
     StringEndIndex = title.index(':')  # Get the index of the first ':' that's found in the String
-    title = (title[1:StringEndIndex])  # Use the index to slice the String accordingly
-    return title
+    return title[1:StringEndIndex]  # Use the index to slice the String accordingly
 
 
 # Loop through every URL in URL_List
-for productURL in URL_List:
-    print(f'{fetchTitleFromProduct(productURL)}: {fetchPriceFromProduct(productURL)}')
+for productUrl in urlList:
+    siteContent = requestWebsite(productUrl)
+    parsedWebsite = parseWebsite(siteContent)
+    title = fetchTitleFromProduct(parsedWebsite)
+    price = fetchPriceFromProduct(parsedWebsite)
+    print(f'{title}: {price}')
